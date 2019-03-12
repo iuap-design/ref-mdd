6d7a16c2494e49b924b790fcd83d8ac0918e8b80
guoyff <guoyff@yonyou.com>
2019-03-12 10:14:11 +0800

    update config

--- packages/mtl-core/rollup.config.js
+++ packages/mtl-core/rollup.config.js
@@ -2,7 +2,7 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
+// import json from 'rollup-plugin-json';
 import pkg from './package.json'
 
 export default {
@@ -19,10 +19,10 @@ export default {
     }
   },
   external: [
-    pkg.dependencies.react,
-    pkg.dependencies['rect-dom']
-    // ...Object.keys(pkg.dependencies || {}),
-    // ...Object.keys(pkg.peerDependencies || {})
+    // pkg.dependencies.react,
+    // pkg.dependencies['rect-dom']
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
   ],
   plugins: [
     resolve(),
@@ -34,20 +34,17 @@ export default {
              '*.json'
       ],// only transpile our source code
     }),
-    json({
-      exclude: [ 'node_modules/**' ]
-    }),
-    json(),
-    commonjs({
-      namedExports: {
-        // left-hand side can be an absolute path, a path
-        // relative to the current directory, or the name
-        // of a module in node_modules
-        'node_modules/react/index.js': [ 'Component' ],
-        'node_modules/tinper-bee/build/tinper-bee.js': [ 'FormControl', 'Button' ]
+    commonjs()
+    // commonjs({
+    //   namedExports: {
+    //     // left-hand side can be an absolute path, a path
+    //     // relative to the current directory, or the name
+    //     // of a module in node_modules
+    //     'node_modules/react/index.js': [ 'Component' ],
+    //     'node_modules/tinper-bee/build/tinper-bee.js': [ 'FormControl', 'Button' ]
         
-      }
-    })
+    //   }
+    // })
     
   ]
 };
