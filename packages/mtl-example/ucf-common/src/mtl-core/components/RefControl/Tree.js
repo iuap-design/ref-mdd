8745b4be3d84bc1bc3b384b76f31d7cddb989e59
izbz wh <731215820@qq.com>
2019-03-15 15:49:13 +0800

    增加树参照

--- packages/mtl-example/ucf-common/src/mtl-core/components/RefControl/Tree.js
+++ packages/mtl-example/ucf-common/src/mtl-core/components/RefControl/Tree.js
@@ -7,9 +7,8 @@ import request from '../../utils/request';
 const noop = () => {
 };
 const getTreeList = (url,param, content="",  jsonp = false) => request(url, {
-	method: 'get',
-	params: Object.assign(param,{content}),
-	jsonp: jsonp
+	method: 'POST',
+	data: param,
 });
 // data:this.treeData,树的所有节点，curKey:正在操作的节点的key值，
 // child:1.request请求得到的该key下的子节点，或者缓存中该key下的子节点
@@ -35,9 +34,9 @@ const propTypes = {
 	matchUrl: PropTypes.string,
 	jsonp: PropTypes.object,
 	headers:PropTypes.object,
-	onMatchInitValue: PropTypes.func,
+	onMatchInitValue: PropTypes.func,//匹配已选中值的回调
 	refModelUrl:PropTypes.object,
-	onAfterAjax: PropTypes.func,
+	onAfterAjax: PropTypes.func,//请求完数据的回调，暂不需要
 };
 const defaultProps = {
 	checkStrictly: false,
@@ -58,7 +57,7 @@ class Tree extends Component {
 		const { checkedArray, valueField } = props;
 		this.state = {
 			showLoading:false,
-			selectedArray: checkedArray || [], //  记录保存的选择项
+			selectedArray: checkedArray || [], //  记录保存的选择项，这个值已于checkedKeys合并
 			checkedKeys: checkedArray.map(item=>{
 				return item[valueField];
 			}),
@@ -98,21 +97,20 @@ class Tree extends Component {
 		}
 	}
 	initComponent = () => {
-		let { matchUrl, param, value, jsonp, headers, checkedArray, onMatchInitValue } = this.props;
+		let { matchUrl, param, value, jsonp, headers, checkedArray, onMatchInitValue,valueField,displayField } = this.props;
 		this.getRefTreeData();
 		//当有已选值，不做校验，即二次打开弹出层不做校验
-		let valueMap = refValParse(value)
-		if(checkedArray.length != 0 || !valueMap.refpk) return;
+		let valueMap = refValParse(value,valueField,displayField)
+		if(checkedArray.length != 0 || !valueMap[valueField]) return;
 		if(matchUrl){
 			request(matchUrl, { 
 				method: 'post',
 				data: {
 					...param,
 					refCode: param.refCode,
-					pk_val: valueMap.refpk.split(',') || ''
+					pk_val: valueMap[valueField].split(',') || ''
 				},
-				jsonp: jsonp,
-				headers
+	
 				
 			}).then(response=>{
 				let { data = [] } = response || {};
@@ -121,16 +119,14 @@ class Tree extends Component {
 				}
 				this.setState({
 					checkedArray: data,
-					selectedArray: data,
 					showLoading: false,
 					checkedKeys: data.map(item=>{
-						return item.refpk;
+						return item[valueField];
 					})
 				});
 			}).catch(()=>{
 				this.setState({
 					checkedArray: [],
-					selectedArray: [],
 					showLoading: false,
 					checkedKeys: []
 				});
@@ -141,7 +137,7 @@ class Tree extends Component {
 				checkedArray: [valueMap],
 				selectedArray: [valueMap],
 				showLoading: false,
-				checkedKeys: valueMap.refpk.split(',')
+				checkedKeys: valueMap[valueField].split(',')
 			});
 		}
 		
@@ -159,9 +155,7 @@ class Tree extends Component {
 				onAfterAjax(res)
 				this.setState({ isAfterAjax: true })
             }
-            res = {"data":[{"code":"org1","children":[{"code":"bj","name":"北京总部-简","pid":"a4cf0601-51e6-4012-9967-b7a64a4b2d47","refcode":"bj","refpk":"5305416e-e7b4-4051-90bd-12d12942295b","id":"5305416e-e7b4-4051-90bd-12d12942295b","isLeaf":"true","refname":"北京总部-简"},{"code":"xd","name":"新道-简","pid":"a4cf0601-51e6-4012-9967-b7a64a4b2d47","refcode":"xd","refpk":"b691afff-ea83-4a3f-affa-beb2be9cba52","id":"b691afff-ea83-4a3f-affa-beb2be9cba52","isLeaf":"true","refname":"新道-简"},{"code":"yy3","name":"test3","pid":"a4cf0601-51e6-4012-9967-b7a64a4b2d47","refcode":"yy3","refpk":"e75694d9-7c00-4e9e-9573-d29465ae79a9","id":"e75694d9-7c00-4e9e-9573-d29465ae79a9","isLeaf":"true","refname":"test3"},{"code":"yy1","name":"test1","pid":"a4cf0601-51e6-4012-9967-b7a64a4b2d47","refcode":"yy1","refpk":"fd32ceeb-57a8-4f44-816e-fa660f5715ab","id":"fd32ceeb-57a8-4f44-816e-fa660f5715ab","isLeaf":"true","refname":"test1"}],"name":"用友集团","refcode":"org1","refpk":"a4cf0601-51e6-4012-9967-b7a64a4b2d47","id":"a4cf0601-51e6-4012-9967-b7a64a4b2d47","refname":"用友集团"}],"page":{"pageSize":100,"currPageIndex":0,"pageCount":0,"totalElements":0},"allpks":null};
-            let { data=[] } = res;
-            
+            let { data=[] } = res.data;       
 			if (data && data.length > 0) {
 				if (lazyModal) {
 					data = data.map((item) => {
@@ -263,8 +257,7 @@ class Tree extends Component {
 			expandedKeys: expandedKeys,
 			onSaveCheckItems: onSaveCheckItems,
         });
-        
-        
+
 		return (
 			<RefTreeBaseUI {...childrenProps}/>
 		);
--- packages/mtl-example/ucf-common/src/mtl-core/render-engine/refer-render/TreeRender.js
+++ packages/mtl-example/ucf-common/src/mtl-core/render-engine/refer-render/TreeRender.js
@@ -7,33 +7,7 @@ import RefWithInput from 'ref-core/lib/refs/refcorewithinput';
 import RefTree from '../../components/RefControl/Tree';
 import 'ref-core/lib/refs/refcorewithinput.css';
 import { connect } from 'mini-store';
-const option = {
-    title: '树',
-    searchable: true,
-    multiple: true,
-    param: {
-        "refCode":"neworganizition_tree",
-    },
-    checkStrictly: true,
-    disabled: false,
-    nodeDisplay: (record) => {
-        return record.refname
-    },
-    displayField: (record) => {
-        return record.refname
-    },//显示内容的键
-    valueField: 'refpk',//真实 value 的键
-    refModelUrl: {
-        treeUrl:'https://mock.yonyoucloud.com/mock/358/blobRefTree',
-        treeUrl: '/pap_basedoc/common-ref/blobRefTree',
-    },
-    matchUrl: '/pap_basedoc/common-ref/matchPKRefJSON',
-    filterUrl: '/pap_basedoc/common-ref/filterRefJSON',
-    lazyModal: false,
-    strictMode: true,
-    lang: 'zh_CN',
-    // className:'ref-walsin-modal'
-};
+
 @connect(state => ({ form: state.form }))
 class TreeRender extends Component {
 
@@ -50,28 +24,58 @@ class TreeRender extends Component {
 
     render() {
         let { store } = this.props;
-        let { viewApplication,refEntity } = store.getState().meta;
+        let { refEntity } = store.getState().meta;
+        const {bMultiSel=false,cCheckFlds='',code,name} = refEntity;
+        // const [valueField,displayField] =  cCheckFlds.split(',');
+        const valueField ='id';
+        const displayField = 'fullname';
+        console.log(displayField,valueField);
         const dataURL =  store.getState().dataUrl;
         const { getFieldError, getFieldProps } = this.props.form;
         // const { cBillName, view } = viewApplication;
-        // const valueField = "id";
-        // const displayField = "{name}";
         // const queryParam = getQueryParam('grid',refEntity,viewApplication);
         // const refModelUrl = {
         //     tableBodyUrl:dataURL
         // }
+
+        const option = {
+            title: name,
+            searchable: true, //默认搜索输入框，没有这个字段
+            multiple: bMultiSel, //refEntity: bMultiSel
+            param: {
+                "refCode":code,//refEntity: code
+            },
+            checkStrictly: true, //没有这个字段
+            nodeDisplay: (record) => { //树节点显示的名字
+                return record[displayField]
+            },
+            displayField: (record) => {//输入框的名字
+                return record[displayField]
+            },//显示内容的键
+            valueField: valueField,//真实 value 的键
+            refModelUrl: {
+                treeUrl: dataURL,
+            },
+            matchUrl: '',//查找已选中的数据的URL,没有这个字段
+            filterUrl: '',//输入框搜索匹配的URL,没有这个字段
+            lazyModal: false,//是否是懒加载树
+            strictMode: true,//是否调用之前缓存的数据，为true则不重新请求
+            lang: 'zh_CN',
+            defaultExpandAll:false
+            // className:'ref-walsin-modal'
+        };
         return (
             <RefWithInput
                 {...option}
                 {...getFieldProps('code1', {
-                    initialValue: JSON.stringify({
-                        code: "org1",
-                        id: "a4cf0601-51e6-4012-9967-b7a64a4b2d47",
-                        name: "用友集团",
-                        refcode: "org1",
-                        refname: "用友集团",
-                        refpk: "a4cf0601-51e6-4012-9967-b7a64a4b2d47",
-                    }),
+                    // initialValue: JSON.stringify({
+                    //     code: "org1",
+                    //     id: "a4cf0601-51e6-4012-9967-b7a64a4b2d47",
+                    //     name: "用友集团",
+                    //     refcode: "org1",
+                    //     refname: "用友集团",
+                    //     refpk: "a4cf0601-51e6-4012-9967-b7a64a4b2d47",
+                    // }),
                     rules: [{
                         message: '请输入请选择', pattern: /[^{"refname":"","refpk":""}|{"refpk":"","refname":""}]/
                     }]
